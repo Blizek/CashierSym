884d0dc751146a390d05f775527458f104f7178f
Blizek <blazej.naziemiec@gmail.com>
2021-05-04 00:00:28 +0200

    cashier view

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -19,8 +20,7 @@ public class Main extends Application {
         stage.setScene(scene);
         stage.setTitle("Cashier Simulator");
         stage.setResizable(false);
-
-
+        stage.getIcons().add(new Image("/images/logo.png"));
 
         stage.show();
     }
